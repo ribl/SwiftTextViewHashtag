6f82634516aa8135c89a5383a79de5986077ba3c
Robert Chen <robert.chen@thorntechnologies.com>
2015-05-08 16:52:56 -0400

    Initial Commit

---
+++ textViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textViewSample
+//
+//  Created by Robert Chen on 5/8/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
